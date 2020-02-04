b87d2f89f930d8e9dd27620c9641468c49754907
Shuichi Tani <muminn4@gmail.com>
2020-02-04 23:19:43 +0900

    add pam_environment

--- .config/i3/init.sh
+++ .config/i3/init.sh
@@ -2,5 +2,8 @@
 
 $HOME/bin/xkeysnail.sh
 
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpgagent
+
 target=$HOME/.config/i3/init.local.sh
 [ -f $target ] && source $target
