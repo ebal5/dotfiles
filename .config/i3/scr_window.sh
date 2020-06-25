a363a343493fcf3af5d541991f3364153bc3e661
Shuichi Tani <muminn4@gmail.com>
2020-06-26 02:12:45 +0900

    update some

--- .config/i3/scr_window.sh
+++ .config/i3/scr_window.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-fn=$(mktemp -p "${HOME}/Pictures/Screenshoots/" -t 'scr.XXXXXX.png')
+tm=$(date -Iseconds)
+fn=$(mktemp -p "${HOME}/Pictures/Screenshoots/" -t "scr_${tm}_XXXX.png")
 xfce4-screenshooter -w -s $fn
